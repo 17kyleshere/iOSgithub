3d196df72eeddd073f2cb428ae56051fff3d1d07
BRANDON KAUFMANN <brandonkaufmann@BRANDON-KAUFMANNs-MacBook-Pro.local>
2014-10-28 14:42:21 -0700

    Initial Commit

---
+++ iOSgithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSgithub
+//
+//  Created by BRANDON KAUFMANN on 10/28/14.
+//  Copyright (c) 2014 Kyle Kaufmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSgithub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSgithub
+//
+//  Created by BRANDON KAUFMANN on 10/28/14.
+//  Copyright (c) 2014 Kyle Kaufmann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
